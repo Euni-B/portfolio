f660dc4d09000e57363ddc7a283e09e8e47e999c
Eunice <ebontreger@gmail.com>
2026-02-17 12:32:27 -0800

    02-16 classwork

---
+++ Week-5/index.js
@@ -0,0 +1,11 @@
+console.log(145890 + 2312131);
+console.log(145890 - 2312131);
+console.log(145890 / 2312131);
+console.log(145890 * 2312131);
+let name= "Eunice"
+
+console.log("name");
+console.log("name");
+console.log("name");
+console.log("name");
+console.log("name");
