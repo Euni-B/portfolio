d622ea8913b15af963c31c84811c13f57cf7c7a7
Eunice <ebontreger@gmail.com>
2026-02-19 10:15:43 -0800

    02-17 classwork

--- Week-5/index.js
+++ Week-5/index.js
@@ -2,10 +2,64 @@ console.log(145890 + 2312131);
 console.log(145890 - 2312131);
 console.log(145890 / 2312131);
 console.log(145890 * 2312131);
+
 let name= "Eunice"
+let age="36"
+let food="pho"
 
 console.log("name");
 console.log("name");
 console.log("name");
 console.log("name");
 console.log("name");
+
+console.log("My name is " +name + " , I am " +age+ " years old, and I love to eat " 
+    +food + "."
+);
+
+function sayHello() {
+console.log("hi")
+console.log("hi")
+console.log("hi")
+console.log("hi")
+}
+
+sayHello();
+
+function addNumbers(a, b){
+console.log(a + b)
+}
+
+addNumbers(2, 3);
+addNumbers(11, 4);
+addNumbers(6, 1);
+
+function deleteNumbers(a, b){
+    console.log(a - b)
+}
+
+deleteNumbers(29 - 3);
+deleteNumbers(11 - 4);
+deleteNumbers(6 - 1);
+
+function multiplyNumbers(a , b){
+    console.log(a * b)
+}
+
+multiplyNumbers(5 , 8)
+multiplyNumbers(9 , 3)
+multiplyNumbers(6 , 8)
+
+function divideNumbers(a , b) {
+    console.log(a / b)
+}
+
+divideNumbers( 84, 6)
+divideNumbers(555, 111)
+divideNumbers(723495, 33)
+
+let favColor= "pink";
+favColor="blue";
+console.log(favColor);
+favColor="green";
+console.log(favColor);
