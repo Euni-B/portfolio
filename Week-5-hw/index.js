ad6e5e320b2c355b5de316e6d4a68e01ad323a0a
Eunice <ebontreger@gmail.com>
2026-02-24 12:39:05 -0800

    02-19.hw

---
+++ Week-5-hw/index.js
@@ -0,0 +1,16 @@
+let name ="Eunice";
+console.log(name);
+
+name = "Yara";
+console.log(name);
+
+const profilePage = "I'm confused";
+
+function sayHello(){
+    alert( "Hello," + name + "!");
+}
+
+function showInfo(){
+  alert("welcome to" + profilePage + "!")
+}
+
