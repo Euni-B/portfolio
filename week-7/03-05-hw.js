5b43b1a630bf279a3f58091e8343274024a273fa
Eunice <ebontreger@gmail.com>
2026-03-08 14:45:22 -0700

    03-05.hw

--- week-7/03-05-hw.js
+++ week-7/03-05-hw.js
@@ -1,9 +1,14 @@
+let color = purple;
+color = document.getElementById("colorInput").value;
+
+
+
 function sayHello() {
     const name = document.getElementById("nameInput").value;
-    alert("Hello "  +  name + "!!!");
+    alert("Hello " + name + "!!!");
 }
 
 function showColor() {
-  const color = document.getElementById("colorInput").value;
-  alert("Your favorite color is " + color);
+    console.log("color");
+    alert("Your favorite color is " + color);
 }
