96c2cb480a70659efbbd1bbdcc2621d4de424467
Eunice <ebontreger@gmail.com>
2026-03-08 14:24:51 -0700

    03-05.hw

---
+++ week-7/03-05-hw.js
@@ -0,0 +1,9 @@
+function sayHello() {
+    const name = document.getElementById("nameInput").value;
+    alert("Hello "  +  name + "!!!");
+}
+
+function showColor() {
+  const color = document.getElementById("colorInput").value;
+  alert("Your favorite color is " + color);
+}
