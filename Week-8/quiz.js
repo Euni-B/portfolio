d52a674505c5048d9d86ac7fba5a7b25efb124be
Eunice <ebontreger@gmail.com>
2026-03-15 00:59:34 -0700

    03-13.hw

--- Week-8/quiz.js
+++ Week-8/quiz.js
@@ -6,9 +6,14 @@ function submitQuiz() {
     const qThree = document.querySelector('input[name="quiz-3"]:checked');
     const qFour = document.querySelector('input[name="quiz-4"]:checked');
     const qFive = document.querySelector('input[name="quiz-5"]:checked');
+    const qSix = document.querySelector('input[name="quiz-6"]:checked');
+    const qSeven = document.querySelector('input[name="quiz-7"]:checked');
+    const qEight = document.querySelector('input[name="quiz-8"]:checked');
+    const qNine = document.querySelector('input[name="quiz-9"]:checked');
+    const qTen = document.querySelector('input[name="quiz-10"]:checked');
 
 
-    const total = 5;
+    const total = 10;
     let score = 0;
 
     // check answers
@@ -36,9 +41,39 @@ function submitQuiz() {
         score++;
     }
 
-// answer 5
+    // answer 5
 
-if (qFive.value === "8") {
+    if (qFive.value === "8") {
+        score++;
+    }
+
+    // question 6
+
+if (qSix.value === "blue") {
+        score++;
+    }
+
+    // question 7
+
+if (qSeven.value === "3.14") {
+        score++;
+    }
+
+    // question 8
+
+if (qEight.value === "2000") {
+        score++;
+    }
+
+    // question 9
+
+if (qNine.value === "adverb") {
+        score++;
+    }
+
+    // question 10
+
+if (qTen.value === "triangle") {
         score++;
     }
 
