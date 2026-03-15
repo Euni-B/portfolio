3e6ed3bf8d23f81b242114e3a6896c33f8198769
Eunice <ebontreger@gmail.com>
2026-03-14 21:50:20 -0700

    03-13.hw

--- Week-8/quiz.js
+++ Week-8/quiz.js
@@ -2,7 +2,11 @@ function submitQuiz() {
     const scoreElement = document.getElementById("score");
     const result = document.getElementById("result");
     const qOne = document.querySelector('input[name="quiz-1"]:checked');
-    const qTwo = document.querySelector('input[name="quiz-1"]:checked');
+    const qTwo = document.querySelector('input[name="quiz-2"]:checked');
+    const qThree = document.querySelector('input[name="quiz-3"]:checked');
+    const qFour = document.querySelector('input[name="quiz-4"]:checked');
+    const qFive = document.querySelector('input[name="quiz-5"]:checked');
+
 
     const total = 5;
     let score = 0;
@@ -16,9 +20,27 @@ function submitQuiz() {
 
     // answer 2
 
-    
+    if (qTwo.value === "4") {
+        score++;
+    }
 
+    // answer 3
 
+    if (qThree.value === "2") {
+        score++;
+    }
+
+    // answer 4
+
+    if (qFour.value === "green") {
+        score++;
+    }
+
+// answer 5
+
+if (qFive.value === "8") {
+        score++;
+    }
 
     // displays to HTML
     scoreElement.textContent = "Score: " + score;
